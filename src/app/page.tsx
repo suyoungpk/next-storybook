e4bd25832b08713d72baee0fbab3e673a89a4839
tty11222 <tty11222@lgupluspartners.co.kr>
2025-07-07 15:36:21 +0900

    이미지경로 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
       <main className={styles.main}>
         <Image
           className={styles.logo}
-          src="/next.svg"
+          src="https://suyoungpk.github.io/next-storybook/next.svg"
           alt="Next.js logo"
           width={180}
           height={38}
@@ -29,7 +29,7 @@ export default function Home() {
           >
             <Image
               className={styles.logo}
-              src="/vercel.svg"
+              src="https://suyoungpk.github.io/next-storybook/vercel.svg"
               alt="Vercel logomark"
               width={20}
               height={20}
@@ -54,7 +54,7 @@ export default function Home() {
         >
           <Image
             aria-hidden
-            src="/file.svg"
+            src="https://suyoungpk.github.io/next-storybook/file.svg"
             alt="File icon"
             width={16}
             height={16}
@@ -68,7 +68,7 @@ export default function Home() {
         >
           <Image
             aria-hidden
-            src="/window.svg"
+            src="https://suyoungpk.github.io/next-storybook/window.svg"
             alt="Window icon"
             width={16}
             height={16}
@@ -82,7 +82,7 @@ export default function Home() {
         >
           <Image
             aria-hidden
-            src="/globe.svg"
+            src="https://suyoungpk.github.io/next-storybook/globe.svg"
             alt="Globe icon"
             width={16}
             height={16}
